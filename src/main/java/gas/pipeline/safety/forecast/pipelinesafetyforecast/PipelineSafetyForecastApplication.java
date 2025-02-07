676aa239f95895121f15b5996c8f46dd117f88bf
patch3 <sowillmove1@gmail.com>
2025-02-07 06:33:06 +0300

    Initialization Commit

---
+++ src/main/java/gas/pipeline/safety/forecast/pipelinesafetyforecast/PipelineSafetyForecastApplication.java
@@ -0,0 +1,11 @@
+package gas.pipeline.safety.forecast.pipelinesafetyforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineSafetyForecastApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineSafetyForecastApplication.class, args);
+    }
+}
---
+++ src/test/java/gas/pipeline/safety/forecast/pipelinesafetyforecast/PipelineSafetyForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package gas.pipeline.safety.forecast.pipelinesafetyforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineSafetyForecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
